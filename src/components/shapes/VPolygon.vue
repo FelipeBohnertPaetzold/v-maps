671b75b29cb6e4bff225e65aa8cdd29051c567e0
Matheus Ribeiro <matt.ribeiro14@outlook.com>
2019-11-28 11:09:07 -0200

    feat: 🎸 add Polygon component
    Add component to draw, edit and show polygons in map

---
+++ src/components/shapes/VPolygon.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+  .p-absolute
+</template>
+
+<script>
+import googleMaps from '../../utils/googleMaps';
+  export default {
+    props: {
+      value: { type: Array, required: true },
+      edition: { type: Boolean, default: false },
+      strokeColor: { type: String, default: '#0f0' },
+      fillColor: { type: String, default: '#0f0' },
+      options: { type: Object, default: () => ({}) }
+    },
+
+    data() {
+      return { polygonRef: {}, infoWindowRef: null}
+    },
+
+    created() {
+      const { paths, options, $parent, strokeColor, fillColor, edition, value } = this
+      this.polygonRef = new window.google.maps.Polygon({
+        ...options,
+        paths: value,
+        map: $parent.map,    
+        strokeColor,
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor,
+        fillOpacity: 0.35,
+        editable: edition,
+        draggable: edition
+      })
+    },
+
+    watch: {
+      value(newValue) {
+        this.polygonRef.setPath(newValue)
+      },
+      
+      edition(newEditionValue) {
+        const paths = []
+        this.polygonRef.setDraggable(newEditionValue)
+        this.polygonRef.setEditable(newEditionValue)
+
+        if (this.polygonRef.getPath(0)) {
+          this.polygonRef.getPath(0).forEach(path => {
+            paths.push({lat: path.lat(), lng: path.lng()})
+          })
+        }
+
+        this.polygonRef.setPath(paths)
+      },
+    },
+
+    destroyed() {
+      this.polygonRef.setMap(null)
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .p-absolute
+    position absolute
+</style>
--- src/lib.js
+++ src/lib.js
@@ -1,13 +1,15 @@
 import VMap from './components/VMap'
 import VMarker from './components/VMarker'
+import VPolygon from './components/shapes/VPolygon'
 import googleMaps from './utils/googleMaps'
 
-export { VMap, VMarker }
+export { VMap, VMarker, VPolygon }
 
 export default {
   install(Vue, params = { apiKey: '', version: '', libraries: [] }) {
     googleMaps.load(params)
     Vue.component('v-map', VMap)
     Vue.component('v-marker', VMarker)
+    Vue.component('v-polygon', VPolygon)
   }
 }
